3bd1192d10f7ca6924a681c394243dd9a0c12b57
wucan <wucan@geo-compass.com>
2017-06-12 19:56:33 +0800

    1.添加屏蔽代码的识别；2.添加类名的提取；3.排除内部方法；4.排除内部属性

--- index.js
+++ index.js
@@ -21,9 +21,26 @@ let handleOne = (file) => {
             if (!trimLine) {
                 continue;
             }
+
+            //排除被屏蔽代码
+            if (trimLine.substring(0, 2) === '//') {
+                continue;
+            }
+
             //如果以下区域被jsdoc管理
             if (trimLine.indexOf('@lends') >= 0) {
+                let lendsIndex = trimLine.indexOf("@lends");
+                let lendsSubstr = trimLine.substring(lendsIndex, trimLine.length);
+                let lendsArr = lendsSubstr.split(' ');
+                let label = "";
+                for (let i = 0; i < lendsArr.length; i++) {
+                    if (lendsArr[i].indexOf("L.") >= 0) {
+                        label = lendsArr[i];
+                        break;
+                    }
+                }
                 obj = {
+                    label: label,
                     methods: [],
                     members: []
                 };
@@ -39,14 +56,19 @@ let handleOne = (file) => {
             //处理function
             if (trimLine.indexOf(': function') >= 0 || trimLine.indexOf(':function') >= 0) {
                 let firstColon = trimLine.indexOf(':');
-                let funcStr = trimLine.substring(0, firstColon);
-                obj.methods.push(funcStr);
+                let funcName = trimLine.substring(0, firstColon);
+                //排除内部方法
+                if (funcName.substring(0, 1) === "_") {
+                    continue;
+                }
+                obj.methods.push(funcName);
+                //排除忽略
                 if (preIgnore) {
                     preIgnore = false;
                     continue;
                 }
                 //处理members
-                if (funcStr === 'initialize' || funcStr === 'initialize') {
+                if (funcName === 'initialize' || funcName === 'initialize') {
                     membersOn = true;
                 } else {
                     membersOn = false;
@@ -59,6 +81,10 @@ let handleOne = (file) => {
                 if (matchResult) {
                     let firstEqual = trimLine.indexOf('=');
                     let member = trimLine.substring(5, firstEqual).trim();
+                    // //排除内部方法
+                    if (member.substring(0, 1) === "_") {
+                        continue;
+                    }
                     obj.members.push(member);
                 }
             }
